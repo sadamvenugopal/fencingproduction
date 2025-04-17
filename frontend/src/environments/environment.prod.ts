8fa9370e1f26caebf9ea409ec9b1e035cdda8259
sadamvenugopal <sadam.venugopal@gmail.com>
2025-04-17 22:35:25 +0530

    prod check

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://ssscg4kwgckck0cswgkokwcs.82.25.109.195.sslip.io/api/booknow' // Public-facing backend URL
+  backendUrl: 'http://ssscg4kwgckck0cswgkokwcs.82.25.109.195.sslip.io' // Public-facing backend URL
 };
