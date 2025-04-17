284cfdfde52f083911f1da9d4a299c2f0bda2b6e
sadamvenugopal <sadam.venugopal@gmail.com>
2025-04-17 22:39:38 +0530

    prod checking

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://ssscg4kwgckck0cswgkokwcs.82.25.109.195.sslip.io' // Public-facing backend URL
+  backendUrl: 'http://fencingproduction:3000' // replace with your backend container name
 };
