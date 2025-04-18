cb3115699092ce87051fc7264ef4c3d70f9547d5
sadamvenugopal <34490704+sadamvenugopal@users.noreply.github.com>
2025-04-18 05:42:13 -0400

    Update booknow.js

--- backend/booknow.js
+++ backend/booknow.js
@@ -8,7 +8,8 @@ const router = express.Router();
 // ✅ Allow only the Angular container to access this API
 //const allowedOrigins = ['http://frontend:4200']; // Replace 'frontend' if your container name is different
 const corsOptions = {
-  origin: 'http://frontend:4200', // Only allow Angular container
+ // origin: 'http://frontend:80', // Only allow Angular container
+ origin:  'http://ssscg4kwgckck0cswgkokwcs.82.25.109.195.sslip.io'
   optionsSuccessStatus: 200,
 };
 
